d0cf2c6c9441c7b9fc9d9f36a1846678a111e7ba
Manoj <hmanoj12@gmail.com>
2024-02-07 08:17:25 +0530

    Monitoring - Prometheus and Grafana2

--- src/main/java/dev/manoj/servicediscovery/ServicediscoveryApplication.java
+++ src/main/java/dev/manoj/servicediscovery/ServicediscoveryApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-//Service discovery server
+//Service discovery server.
 public class ServicediscoveryApplication {
 
 	public static void main(String[] args) {
